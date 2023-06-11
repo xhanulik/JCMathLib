e29a648a57eb7b8adfd02e067e633a13b6ad9681
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-06-11 18:15:12 +0200

    test: add testin Run main

--- applet/src/main/java/main/Run.java
+++ applet/src/main/java/main/Run.java
@@ -6,6 +6,8 @@ import javacard.framework.AID;
 import opencrypto.jcmathlib.UnitTests;
 import org.bouncycastle.util.encoders.Hex;
 import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import java.util.Arrays;
 
 public class Run {
     public static void main(String[] args) throws Exception {
@@ -16,6 +18,10 @@ public class Run {
 
         simulator.selectApplet(appletAID);
 
-        System.out.println(Hex.toHexString(simulator.transmitCommand(new CommandAPDU(0, 0, 0, 0)).getData()));
+        CommandAPDU commandAPDU = new CommandAPDU(0xB0, 0x21, 0x05, 0x00, new byte[]{0x01, 0x02, 0x03, 0x04,0x05});
+        ResponseAPDU response = simulator.transmitCommand(commandAPDU);
+
+        System.out.printf("INS: %x, P1: %x, P2: %x, DATA: %s\n", commandAPDU.getINS(), commandAPDU.getP1(), commandAPDU.getP2(), Arrays.toString(commandAPDU.getData()));
+        System.out.printf("RES: %2x %2x, DATA: %s\n", response.getSW1(), response.getSW2(), new String(response.getData()));
     }
 }
