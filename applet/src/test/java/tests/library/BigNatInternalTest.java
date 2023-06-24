90905cc786d72744f8962817188bd0427e0f59de
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-06-24 11:42:45 +0200

    tests: adjust uni tests for copy

--- applet/src/test/java/tests/library/BigNatInternalTest.java
+++ applet/src/test/java/tests/library/BigNatInternalTest.java
@@ -134,10 +134,8 @@ public class BigNatInternalTest {
 
         byte[] data1 = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
         bn1.fromByteArray(data1, (short) 0, (short) data1.length);
-        byte[] data2 = {0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
+        byte[] data2 = {0x00, 0x00, 0x00, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
         bn2.fromByteArray(data2, (short) 0, (short) data2.length);
-        bn2.resize((short) (data1.length + 3));
-
         bn1.copy(bn2);
         bn1.shrink();
         bn2.shrink();
