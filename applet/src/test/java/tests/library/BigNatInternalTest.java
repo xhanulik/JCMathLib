23b9e91fd9d1a0eb6d6cb272df4597822e1c2159
Veronika Hanulíková <xhanulik@fi.muni.cz>
2023-06-28 22:14:40 +0200

    tests: add unit tests for add method

--- applet/src/test/java/tests/library/BigNatInternalTest.java
+++ applet/src/test/java/tests/library/BigNatInternalTest.java
@@ -587,20 +587,6 @@ public class BigNatInternalTest {
     }
 
     @Test
-    public void add_noShift() {
-        ResourceManager rm = new ResourceManager((short) 256);
-        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
-        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
-        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
-
-        byte[] data1 = {0x01, 0x7f};
-        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
-        byte[] data2 = {0x7f, 0x7f};
-        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
-        bn1.add(bn2);
-    }
-
-    @Test
     public void increment_firstByte_noOverflow() {
         ResourceManager rm = new ResourceManager((short) 256);
         byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
@@ -692,5 +678,157 @@ public class BigNatInternalTest {
         bn2.fromByteArray(new byte[]{(byte) 0x00, (byte) 0xff, (byte) 0xff, (byte) 0xff}, (short) 0, (short) 4);
         Assertions.assertTrue(bn1.equals_original(bn2));
         Assertions.assertEquals(4, bn1.length());
+    }
+
+    @Test
+    public void add_sameLength_noShift_noOverflow() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x02};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {0x03, 0x04};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x04, 0x06};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_sameLength_noShift_overflow() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x02};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {(byte) 0xff, 0x04};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        short carry = bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x00, 0x06};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_thisLonger_noShift_noOverflow() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x01, 0x02};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {0x03, 0x04};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x01, 0x04, 0x06};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_thisLonger_noShift_overflow() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x01, 0x02};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {(byte) 0xff, 0x04};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x02, 0x00, 0x06};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_thisLonger_noShift_overflow2() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x01, 0x02};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {(byte) 0xff, (byte) 0xff};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x02, 0x01, 0x01};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_thisLonger_noShift_overflow3() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x08, (byte) 0xff};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {(byte) 0xff, (byte) 0x09};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x02, 0x08, 0x08};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_thisLonger_noShift_overflow4() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {0x01, 0x08, (byte) 0xff};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {(byte) 0xff, (byte) 0xff};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x02, 0x08, (byte) 0xfe};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
+    }
+
+    @Test
+    public void add_otherLonger_noShift_noOverflow() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
+
+        byte[] data1 = {(byte) 0xff, 0x04};
+        bn1.fromByteArray(data1, (short) 0, (short) data1.length);
+        byte[] data2 = {0x01, 0x01, 0x02};
+        bn2.fromByteArray(data2, (short) 0, (short) data2.length);
+        bn1.add(bn2);
+
+        BigNat bn3 = new BigNat((short) 10, memoryType, rm);
+        byte[] data3 = {0x00, 0x06};
+        bn3.fromByteArray(data3, (short) 0, (short) data3.length);
+        Assertions.assertTrue(bn1.equals(bn3));
     }
 }
