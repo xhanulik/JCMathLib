d95c561776582a6fe399b27ecf0e5194af507f9a
Veronika Hanulíková <xhanulik@fi.muni.cz>
2024-02-08 18:42:06 +0100

    test(shrink): Add tests for shrink method

--- applet/src/test/java/tests/BigNatInternal/ShrinkTest.java
+++ applet/src/test/java/tests/BigNatInternal/ShrinkTest.java
@@ -1,2 +1,34 @@
-package tests.BigNatInternal;public class ShrinkTest {
+package tests.BigNatInternal;
+
+import javacard.framework.JCSystem;
+import opencrypto.jcmathlib.BigNat;
+import opencrypto.jcmathlib.BigNatInternal;
+import opencrypto.jcmathlib.ResourceManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShrinkTest {
+    @Test
+    public void shrink_longer() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
+
+        byte[] data = {0x00, 0x00, 0x00, 0x01, 0x02, 0x03};
+        bn.fromByteArray(data, (short) 0, (short) data.length);
+        bn.shrink();
+        Assertions.assertEquals(3, bn.length());
+    }
+
+    @Test
+    public void shrink_sameLength() {
+        ResourceManager rm = new ResourceManager((short) 256);
+        byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
+        BigNat bn = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
+
+        byte[] data = {0x01, 0x02, 0x03};
+        bn.fromByteArray(data, (short) 0, (short) data.length);
+        bn.shrink();
+        Assertions.assertEquals(3, bn.length());
+    }
 }
