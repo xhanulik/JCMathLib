700abb274f3030d6df4f60bc744b6665057e11e4
Veronika Hanulíková <xhanulik@fi.muni.cz>
2024-02-17 16:38:35 +0100

    test(copy): adjusts unit tests for copy method

--- applet/src/test/java/tests/BigNatInternal/CopyTest.java
+++ applet/src/test/java/tests/BigNatInternal/CopyTest.java
@@ -12,8 +12,8 @@ public class CopyTest {
     public void copy_empty() {
         ResourceManager rm = new ResourceManager((short) 256);
         byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
-        BigNat bn1 = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
-        BigNat bn2 = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
 
         byte[] data1 = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
         bn1.fromByteArray(data1, (short) 0, (short) data1.length);
@@ -29,15 +29,15 @@ public class CopyTest {
     public void copy_thisLonger() {
         ResourceManager rm = new ResourceManager((short) 256);
         byte memoryType = JCSystem.MEMORY_TYPE_TRANSIENT_RESET;
-        BigNat bn1 = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
-        BigNat bn2 = new BigNat(rm.MAX_BIGNAT_SIZE, memoryType, rm);
+        BigNat bn1 = new BigNat((short) 10, memoryType, rm);
+        BigNat bn2 = new BigNat((short) 10, memoryType, rm);
 
         byte[] data1 = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
         bn1.fromByteArray(data1, (short) 0, (short) data1.length);
         byte[] data2 = {0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
         bn2.fromByteArray(data2, (short) 0, (short) data2.length);
 
-        bn1.copy_original(bn2);
+        bn1.copy(bn2);
         bn1.shrink_original();
         Assertions.assertTrue(bn1.equals_original(bn2));
     }
@@ -53,7 +53,7 @@ public class CopyTest {
         bn1.fromByteArray(data1, (short) 0, (short) data1.length);
         byte[] data2 = {0x00, 0x00, 0x00, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
         bn2.fromByteArray(data2, (short) 0, (short) data2.length);
-        bn1.copy_original(bn2);
+        bn1.copy(bn2);
         bn1.shrink_original();
         bn2.shrink_original();
         Assertions.assertTrue(bn1.equals_original(bn2));
@@ -70,7 +70,7 @@ public class CopyTest {
         bn1.fromByteArray(data1, (short) 0, (short) data1.length);
         byte[] data2 = {0x00, 0x00, 0x00, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
         bn2.fromByteArray(data2, (short) 0, (short) data2.length);
-        bn1.copy_original(bn2);
+        bn1.copy(bn2);
         bn1.shrink_original();
         bn2.shrink_original();
         Assertions.assertTrue(bn1.equals_original(bn2));
@@ -88,6 +88,6 @@ public class CopyTest {
         byte[] data2 = {0x0a, 0x0b, 0x0c, 0x0d, 0x0e};
         bn2.fromByteArray(data2, (short) 0, (short) data2.length);
 
-        Assertions.assertThrows(ISOException.class, () -> bn1.copy_original(bn2));
+        Assertions.assertThrows(ISOException.class, () -> bn1.copy(bn2));
     }
 }
